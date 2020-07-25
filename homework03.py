48f53991f83219f7c2e0340cad7e6fa6682d2d67
totets <67834158+totets@users.noreply.github.com>
2020-07-25 18:46:41 +0900

    genie_scraping complete
    mongo update completed

--- homework03.py
+++ homework03.py
@@ -1,16 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
+from pymongo import MongoClient  # pymongo를 임포트 하기(패키지 설치 먼저 해야겠죠?)
+client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+db = client.dbsparta  # 'dbsparta'라는 이름의 db를 만듭니다.
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-# 아래 빈 칸('')을 채워보세요
-data = requests.get('', headers=headers)
+
+data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20200713', headers=headers)
 soup = BeautifulSoup(data.text, 'html.parser')
 
 trs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
-# 아래 빈 칸('')을 채워보세요
+
 for tr in trs:
-    rank = tr.select_one('').text[0:2].strip()
-    title = tr.select_one('').text.strip()
-    artist = tr.select_one('').text
-    print(rank, title, artist)
+    rank = tr.select_one('td.number').text[0:2].strip()
+    title = tr.select_one('td.info > a.title.ellipsis').text.strip()
+    artist = tr.select_one('td.info > a.artist.ellipsis').text
+    # print(rank, title, artist)
+
+    data = {'rank': rank, 'title': title, 'artist': artist}
+    db.genie_chart.insert_one(data)
